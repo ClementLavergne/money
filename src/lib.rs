3959e793bb8622a45dd1f05137d99bd86b808310
Clément Lavergne-Prudence <lavergne.clement@icloud.com>
2020-08-14 22:25:19 +0200

    provide functions related to tags management
    - Define a singleton Account which manages available tags only
    - Add panic hook from Account's constructor to enhance debuggging from console
    - Provide API for adding, removing and getting tags
    - Unit testing of related functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,126 @@
+//! # Money
+//!
+//! `money` is a collection of utilities to make tracking money expenses.
+
 mod utils;
 
 use wasm_bindgen::prelude::*;
+
+extern crate js_sys;
+use js_sys::Array;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+/// Manages account data.
+#[wasm_bindgen]
+pub struct Account {
+    available_tags: Vec<String>,
+}
+
+#[wasm_bindgen]
+impl Account {
+    /// Instantiates a new object.
+    #[wasm_bindgen(constructor)]
+    pub fn create() -> Account {
+        utils::set_panic_hook();
+
+        let empty_tags = Vec::new();
+
+        Account {
+            available_tags: empty_tags,
+        }
+    }
+
+    /// Adds a new tag if not exists yet.
+    pub fn add_tag(&mut self, tag: &str) {
+        if !tag.is_empty() {
+            if self.available_tags.iter().any(|i| i == tag) {
+                println!("tag '{}' already exists!", tag)
+            } else {
+                self.available_tags.push(tag.to_string())
+            }
+        }
+    }
+
+    /// Removes an existing tag.
+    pub fn remove_tag(&mut self, tag: &str) {
+        let index = self.available_tags.iter().position(|x| x == tag);
+        if let Some(i) = index {
+            self.available_tags.remove(i);
+        }
+    }
+
+    /// Extracts available tags as `JsValue`.
+    pub fn get_tags(&self) -> Array {
+        self.available_tags.iter().map(JsValue::from).collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ignore_empty_tag() {
+        let mut data = Account::create();
+
+        assert_eq!(data.available_tags.len(), 0);
+        data.add_tag("");
+        assert_eq!(data.available_tags.len(), 0);
+    }
+
+    #[test]
+    fn avoid_tag_redundancy() {
+        let mut data = Account::create();
+
+        assert_eq!(data.available_tags.len(), 0);
+        data.add_tag("Food");
+        assert_eq!(vec![String::from("Food")], data.available_tags);
+        data.add_tag("Transport");
+        assert_eq!(
+            vec![String::from("Food"), String::from("Transport")],
+            data.available_tags
+        );
+        data.add_tag("Food");
+        data.add_tag("Service");
+        assert_eq!(
+            vec![
+                String::from("Food"),
+                String::from("Transport"),
+                String::from("Service")
+            ],
+            data.available_tags
+        );
+    }
+
+    #[test]
+    fn safe_tag_remove() {
+        let mut data = Account::create();
+
+        data.add_tag("Food");
+        data.add_tag("Transport");
+        data.add_tag("Service");
+        assert_eq!(
+            vec![
+                String::from("Food"),
+                String::from("Transport"),
+                String::from("Service")
+            ],
+            data.available_tags
+        );
+        data.remove_tag("Food");
+        assert_eq!(
+            vec![String::from("Transport"), String::from("Service")],
+            data.available_tags
+        );
+        data.remove_tag("Hangout");
+        assert_eq!(
+            vec![String::from("Transport"), String::from("Service")],
+            data.available_tags
+        );
+    }
+}
--- tests/web.rs
+++ tests/web.rs
@@ -2,7 +2,30 @@
 
 #![cfg(target_arch = "wasm32")]
 
+use wasm_bindgen::prelude::JsValue;
+
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
+extern crate money;
+use money::Account;
+extern crate js_sys;
+use js_sys::Array;
 
 wasm_bindgen_test_configure!(run_in_browser);
+
+#[wasm_bindgen_test]
+pub fn extract_tag_array() {
+    let mut data = Account::create();
+
+    data.add_tag("Food");
+    data.add_tag("Transport");
+    data.add_tag("Service");
+
+    let output_array = data.get_tags();
+    let expected_array = Array::new();
+
+    expected_array.set(0, JsValue::from("Food"));
+    expected_array.set(1, JsValue::from("Transport"));
+    expected_array.set(2, JsValue::from("Service"));
+    assert_eq!(expected_array.to_vec(), output_array.to_vec());
+}
