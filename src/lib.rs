6a5cbf1784b75ff8b250c091b35c31c5296ce465
Clément Lavergne-Prudence <lavergne.clement@icloud.com>
2020-08-20 17:59:26 +0200

    rename "available_tags" as "tags"

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 /// Manages account data.
 #[wasm_bindgen]
 pub struct Account {
-    available_tags: Vec<String>,
+    tags: Vec<String>,
 }
 
 #[wasm_bindgen]
@@ -28,35 +28,31 @@ impl Account {
     pub fn create() -> Account {
         utils::set_panic_hook();
 
-        let empty_tags = Vec::new();
-
-        Account {
-            available_tags: empty_tags,
-        }
+        Account { tags: Vec::new() }
     }
 
     /// Adds a new tag if not exists yet.
     pub fn add_tag(&mut self, tag: &str) {
         if !tag.is_empty() {
-            if self.available_tags.iter().any(|i| i == tag) {
+            if self.tags.iter().any(|i| i == tag) {
                 println!("tag '{}' already exists!", tag)
             } else {
-                self.available_tags.push(tag.to_string())
+                self.tags.push(tag.to_string())
             }
         }
     }
 
     /// Removes an existing tag.
     pub fn remove_tag(&mut self, tag: &str) {
-        let index = self.available_tags.iter().position(|x| x == tag);
+        let index = self.tags.iter().position(|x| x == tag);
         if let Some(i) = index {
-            self.available_tags.remove(i);
+            self.tags.remove(i);
         }
     }
 
     /// Extracts available tags as `JsValue`.
     pub fn get_tags(&self) -> Array {
-        self.available_tags.iter().map(JsValue::from).collect()
+        self.tags.iter().map(JsValue::from).collect()
     }
 }
 
@@ -68,22 +64,22 @@ mod tests {
     fn ignore_empty_tag() {
         let mut data = Account::create();
 
-        assert_eq!(data.available_tags.len(), 0);
+        assert_eq!(data.tags.len(), 0);
         data.add_tag("");
-        assert_eq!(data.available_tags.len(), 0);
+        assert_eq!(data.tags.len(), 0);
     }
 
     #[test]
     fn avoid_tag_redundancy() {
         let mut data = Account::create();
 
-        assert_eq!(data.available_tags.len(), 0);
+        assert_eq!(data.tags.len(), 0);
         data.add_tag("Food");
-        assert_eq!(vec![String::from("Food")], data.available_tags);
+        assert_eq!(vec![String::from("Food")], data.tags);
         data.add_tag("Transport");
         assert_eq!(
             vec![String::from("Food"), String::from("Transport")],
-            data.available_tags
+            data.tags
         );
         data.add_tag("Food");
         data.add_tag("Service");
@@ -93,7 +89,7 @@ mod tests {
                 String::from("Transport"),
                 String::from("Service")
             ],
-            data.available_tags
+            data.tags
         );
     }
 
@@ -110,17 +106,17 @@ mod tests {
                 String::from("Transport"),
                 String::from("Service")
             ],
-            data.available_tags
+            data.tags
         );
         data.remove_tag("Food");
         assert_eq!(
             vec![String::from("Transport"), String::from("Service")],
-            data.available_tags
+            data.tags
         );
         data.remove_tag("Hangout");
         assert_eq!(
             vec![String::from("Transport"), String::from("Service")],
-            data.available_tags
+            data.tags
         );
     }
 }
